31b54c6188b1e392036e4dca360ae426c0325b48
VanSharine <vanessasharine@gmail.com>
2020-10-05 15:48:19 -0300

    Exercicio Fortune Cookies versão inicial

--- src/fortuneCookies.kt
+++ src/fortuneCookies.kt
@@ -1,14 +1,33 @@
 fun main(){
-    getFortuneCookie()
+    //getFortuneCookie()
+    println("\nYour fortune is: ${getFortuneCookie()}")
+
+    var fortune:String = "5"
+    for (i in 1..7){
+        println("\nYour fortune is: $fortune")
+        if(fortune.contains("Take it easy")) break
+    }
 }
-fun getFortuneCookie():String{
+fun getFortuneCookie():String {
     val fortune = listOf("You will have a great day!", "Things will go well for you today.",
     "Enjoy a wonderful day of success.", "Be humble and all will turn out well.",
     "Today is a good day for exercising restraint.", "Take it easy and enjoy life!",
     "Treasure your friends because they are your greatest fortune.")
 
-    print("Enter your birthday")
-    var birthday: Int = readLine()
+    print("\nEnter your birthday")
+    val birthday =  readLine()?.toIntOrNull()?:1
+    /*val birthday: String? = readLine()
+    birthday?.toIntOrNull()?:println("Digite o dia do seu aniversário")
+    //birthday ?:println("Digite o dia do seu aniversário")
+    */
+    return fortune[birthday.rem(fortune.size)]
+    /*
+    val index = birthday % 7
+
+    for ((index, value) in fortune.withIndex()) {
+        println("1 ${sizes[i]} = ${value.toLong()} bytes")
+    }*/
 
 
 }
+
